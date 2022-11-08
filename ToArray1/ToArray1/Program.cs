b325903fcdf48d8747d01b45ea11acd76ce99b99
Akira Kawamata <autumn@piedey.co.jp>
2022-11-08 11:53:51 +0900

    add ToArray1

---
+++ ToArray1/ToArray1/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] a = { 1, 2, 3 };
+IEnumerable<int> b = a;
+IEnumerable<int> c = a.ToArray();
+Console.WriteLine($"b.Sum()={b.Sum()}");
+Console.WriteLine($"c.Sum()={c.Sum()}");
+a[0] = 2;
+Console.WriteLine($"b.Sum()={b.Sum()}");
+Console.WriteLine($"c.Sum()={c.Sum()}");
