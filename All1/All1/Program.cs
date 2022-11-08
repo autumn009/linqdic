e2b31460dfb24a280d5652f990aa173e01935903
Akira Kawamata <autumn@piedey.co.jp>
2022-11-08 18:23:20 +0900

    add All1

---
+++ All1/All1/Program.cs
@@ -0,0 +1,14 @@
+﻿sub(new int[] { 1, 2, 3 });
+sub(new int[] { 1, -2, 3 });
+sub(new int[] { 1, -2, -3 });
+
+void sub(int[] array)
+{
+	Console.Write($"【{string.Join(',', array)}は全て正数】");
+	if (array.All(c => c >= 0))
+		Console.WriteLine("です。");
+	else
+		Console.WriteLine("ではありません。");
+}
+
+
