19184f1ae8165d79493b4ad9b195415c6624bffb
Akira Kawamata <autumn@piedey.co.jp>
2022-11-08 20:22:42 +0900

    add Append1

---
+++ Append1/Append1/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] a = { 1, 2 };
+IEnumerable<int> b = a.Append(3);
+
+sub("a", a);
+sub("b", b);
+
+void sub(string name, IEnumerable<int> x)
+{
+    Console.WriteLine($"{name}={string.Join(',',x)}");
+}
