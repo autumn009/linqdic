c9d6c96a6a0be1a7ae5d59207c526a80b7e172d8
Akira Kawamata <autumn@piedey.co.jp>
2022-11-08 19:40:06 +0900

    add Any1

---
+++ Any1/Any1/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] ar = {
+    "Jack Sparrow",
+    "Jack the Ripper",
+    "Jean-Claude"
+};
+if( ar.Any(c=>c.Contains("Jack")))
+    Console.WriteLine("この中に一人以上のJackがいる!");
+else
+    Console.WriteLine("Jackは一人もいません。");
