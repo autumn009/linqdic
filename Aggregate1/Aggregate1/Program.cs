075a940583ab45d62fa3ba1d27ec3cd54c8f65e3
Akira Kawamata <autumn@piedey.co.jp>
2022-11-08 15:17:33 +0900

    add Aggregate1

---
+++ Aggregate1/Aggregate1/Program.cs
@@ -0,0 +1,10 @@
+﻿var words = "The quick brown fox jumps over the lazy dog".Split(' ');
+var s = words.Aggregate("Result with in 40 chars: ",(tempstr, word) =>
+{
+    if (tempstr.Length > 40)
+        return tempstr;
+    else
+        return tempstr + " " + word;
+});
+Console.WriteLine($"s=\"{s}\"");
+Console.WriteLine($"s.Length={s.Length}");
