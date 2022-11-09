e576f048642e91b10b662c0ff95004b708358ebb
Akira Kawamata <autumn@piedey.co.jp>
2022-11-09 13:37:49 +0900

    add Avarage1

---
+++ Average1/Average1/Program.cs
@@ -0,0 +1,15 @@
+﻿decimal[] d = { 1.1m, 1.2m, 1.3m };
+Console.WriteLine(d.Average());
+
+int?[] a = { 1, null, 2 };
+Console.WriteLine(a.Average());
+
+double?[] b = { null, null, null };
+Console.WriteLine(b.Average()==null);
+
+Tuple<string, int>[] tuples = {
+    new Tuple<string,int>("Alice",2),
+    new Tuple<string,int>("Betty",3),
+    new Tuple<string,int>("Cindy",4),
+};
+Console.WriteLine(tuples.Average(c=>c.Item2));
