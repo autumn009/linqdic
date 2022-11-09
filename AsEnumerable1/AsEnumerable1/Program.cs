78c922f05c42999840541e47c69562e52e250d29
Akira Kawamata <autumn@piedey.co.jp>
2022-11-09 10:54:28 +0900

    add AsEnumerable1

---
+++ AsEnumerable1/AsEnumerable1/Program.cs
@@ -0,0 +1,14 @@
+﻿var qParallel = Enumerable.Range(1, 20).AsParallel();
+var qSerial = qParallel.AsEnumerable();
+Console.Write("By Parallel: ");
+foreach (var item in qParallel.Select(c => workAsync(c))) Console.Write($"{item} ");
+Console.WriteLine();
+Console.Write("By Serial: ");
+foreach (var item in qSerial.Select(c => workAsync(c))) Console.Write($"{item} ");
+Console.WriteLine();
+
+static int workAsync(int x)
+{
+    Thread.Sleep(Random.Shared.Next(100));
+    return x;
+}
