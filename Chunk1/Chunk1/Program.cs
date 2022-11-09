acce18c01a81f9713bdabce16af4e702515f10f6
Akira Kawamata <autumn@piedey.co.jp>
2022-11-09 17:31:27 +0900

    add Chunk1

---
+++ Chunk1/Chunk1/Program.cs
@@ -0,0 +1,7 @@
+﻿IEnumerable<int> a = Enumerable.Range(0, 10);
+IEnumerable<int[]> x = a.Chunk(4);
+foreach (var item in x)
+{
+	foreach (var item2 in item) Console.Write($"{item2} ");
+	Console.WriteLine();
+}
