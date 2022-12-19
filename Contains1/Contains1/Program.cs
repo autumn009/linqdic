47b6608046712b7a19812cf4798d8a4fca1a1ebc
Akira Kawamata <autumn@piedey.co.jp>
2022-12-19 19:31:51 +0900

    add Contains1

---
+++ Contains1/Contains1/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] array = { 1, 2, 3, 4 };
+int[] targets = { 3, 5 };
+foreach (var item in targets)
+{
+    string msg = array.Contains(item) ? "います" : "いません";
+    Console.WriteLine($"{string.Join(',',array)}に{item}は含まれて{msg}");
+}
+
