aa4113926193f639fb3c4acd92a3dd500967aa00
Akira Kawamata <autumn@piedey.co.jp>
2022-12-20 14:59:55 +0900

    add DistinctBy1

---
+++ DistinctBy1/DistinctBy1/Program.cs
@@ -0,0 +1,11 @@
+﻿Person[] p = {
+    new Person("Taro",new DateTime(2023,1,1)),
+    new Person("Taro",new DateTime(2023,1,2)),
+    new Person("Jiro",new DateTime(2023,1,3)),
+    new Person("Taro",new DateTime(2023,1,4)),
+    new Person("Saburo",new DateTime(2023,1,5)),
+}; 
+
+Console.WriteLine(string.Join(',',p.DistinctBy(c=>c.Name).Select(c=>c.Name)));
+
+record Person(string Name, DateTime Time);
