0029127aa4f1026876c08e747bbcaa5374d8b869
Akira Kawamata <autumn@piedey.co.jp>
2022-12-20 11:02:54 +0900

    add Count1

---
+++ Count1/Count1/Program.cs
@@ -0,0 +1,7 @@
+﻿List<int> a = new List<int> { 1, 2, 3, 4 };
+
+Console.WriteLine($"LINQのカウントメソッドで要素数: {a.Count()}");
+Console.WriteLine($"Listクラスのプロパティで要素数: {a.Count}");
+
+Console.WriteLine($"Countメソッドのみで2以上の要素数: {a.Count(c=>c >= 2)}");
+Console.WriteLine($"WhereメソッドとCountメソッドで2以上の要素数: {a.Where(c => c >= 2).Count()}");
