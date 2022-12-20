327d7937d1aed75e9a4742b662aad8cecabf5d92
Akira Kawamata <autumn@piedey.co.jp>
2022-12-20 15:46:57 +0900

    add last

---
+++ ElementAt1/ElementAt1/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] a = { "Taro", "Jiro", "Saburo", "Siro" };
+
+// 配列の添え字を使う
+Console.WriteLine(a[2]);
+
+// LINQ(ElementAt)を使う
+Console.WriteLine(a.ElementAt(2));
+
+// LINQ(ElementAtOrDefault)を使う
+Console.WriteLine(a.ElementAtOrDefault(4));
+
+// 最後の1つを取得する
+Console.WriteLine(a.ElementAt(^1));
