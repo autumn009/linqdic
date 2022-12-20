68c9d3d0bf0c10062ecf588c0c5fc57214421170
Akira Kawamata <autumn@piedey.co.jp>
2022-12-20 11:33:19 +0900

    add comment for LongCount1

--- LongCount1/LongCount1/Program.cs
+++ LongCount1/LongCount1/Program.cs
@@ -1,4 +1,6 @@
 ﻿Console.WriteLine($"シーケンスのカウントは{GetLongEnum().LongCount()}");
+// 以下の行は例外を起こし実行できない
+// Console.WriteLine($"シーケンスのカウントは{GetLongEnum().Count()}");
 
 IEnumerable<long> GetLongEnum()
 {
