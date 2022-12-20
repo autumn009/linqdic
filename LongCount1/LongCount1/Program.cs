ef27fe1116266bd3c4d13d2224af4be4cc6fddcc
Akira Kawamata <autumn@piedey.co.jp>
2022-12-20 11:30:51 +0900

    add LongCoint1

---
+++ LongCount1/LongCount1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine($"シーケンスのカウントは{GetLongEnum().LongCount()}");
+
+IEnumerable<long> GetLongEnum()
+{
+	for (long i = 0L; i < (long)int.MaxValue+1L; i++)
+	{
+		yield return i;
+	}
+}
