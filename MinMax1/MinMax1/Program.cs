1467947ef8c8e7ab9a70071c43e9b3108cd5f25b
Akira Kawamata <autumn@piedey.co.jp>
2022-12-21 17:16:37 +0900

    add MinMax1

---
+++ MinMax1/MinMax1/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] prices = { 100, 200, 500, 300 };
+Console.WriteLine($"{string.Join(',',prices)}の中で、");
+Console.WriteLine($"最小の値段は{prices.Min()}");
+Console.WriteLine($"最大の値段は{prices.Max()}");
