fe4f10acf7c683fe6a2a570a235b57d5498955fb
Akira Kawamata <autumn@piedey.co.jp>
2022-12-21 18:03:34 +0900

    add MinMax3

---
+++ MinMax3/MinMax3/Program.cs
@@ -0,0 +1,11 @@
+﻿Person[] persons =
+{
+    new Person("Taro",20),
+    new Person("Jiro",20),
+    new Person("Saburo",20),
+};
+
+var r = persons.MaxBy(c=>c.Age);
+Console.WriteLine($"選ばれたのは{r.Name}");
+
+record Person(string Name, int Age);
