72197ee0fcf81643ec22548c44277487979ca5dc
Akira Kawamata <autumn@piedey.co.jp>
2022-12-21 20:09:31 +0900

    add Order

---
+++ Order1/Order1/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] a = { 2, 3, 1 };
+Console.WriteLine($"オリジナルデータ: {string.Join(',',a)}");
+Console.WriteLine($"Orderメソッド: {string.Join(',', a.Order())}");
+Console.WriteLine($"Array.Sortメソッド: {string.Join(',', byArray(a.ToArray()))}");
+Console.WriteLine($"List.Sortメソッド: {string.Join(',', byList(a.ToList()))}");
+
+int[] byArray(int[] ar)
+{
+    Array.Sort(ar);
+    return ar;
+}
+
+List<int> byList(List<int> list)
+{
+    list.Sort();
+    return list;
+}
---
+++ Order2/Order2/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Linq;
+
+int[] a = { 2, 3, 1 };
+Console.WriteLine($"オリジナルデータ: {string.Join(',', a)}");
+Console.WriteLine($"OrderByメソッド: {string.Join(',', a.OrderBy(c=>c))}");
+
+
