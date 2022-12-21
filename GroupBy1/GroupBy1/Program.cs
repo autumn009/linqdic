34485af511b776e5d943ed490685ee1696dae796
Akira Kawamata <autumn@piedey.co.jp>
2022-12-21 11:09:51 +0900

    add GroupBy1

---
+++ GroupBy1/GroupBy1/Program.cs
@@ -0,0 +1,13 @@
+﻿Person[] p =
+{
+    new Person(1,"一之瀬"),
+    new Person(2,"二階堂"),
+    new Person(1,"一橋"),
+};
+foreach (var item in p.GroupBy(c => c.Grade))
+{
+    Console.WriteLine("{0}年生グループ", item.Key);
+    foreach (var item2 in item) Console.WriteLine(item2.Name);
+}
+
+record Person(int Grade,string Name);
