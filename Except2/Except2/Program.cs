545eb13f8d9cb83a8f24d9ffe262e776b17cf771
Akira Kawamata <autumn@piedey.co.jp>
2022-12-21 10:18:57 +0900

    add Except2

---
+++ Except2/Except2/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] a = { 1, 2, 3, 4 };
+object[] b = { 1, 3, 5, 7 };
+// ↓コンパイルできない
+Console.WriteLine($"{string.Join(',', a)}から" +
+    $"{string.Join(',', b)}を除外すると" +
+    $"{string.Join(',', a.Except(b))}");
