fb3eba552ea8a225cc28016ea3993ec153b812ab
Akira Kawamata <autumn@piedey.co.jp>
2022-12-21 14:39:46 +0900

    add Intersect1

---
+++ Intersect1/Intersect1/Program.cs
@@ -0,0 +1,4 @@
+﻿string[] danceClub = { "Hanako", "Misa", "Eriko" };
+string[] ikebanaClub = { "Eriko", "Misuzu", "Misa", "Michie" };
+
+Console.WriteLine($"ダンスクラブと生け花クラブの両方に所属しているのは{string.Join(',',danceClub.Intersect(ikebanaClub))}。");
