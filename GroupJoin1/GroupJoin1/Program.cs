af1e2c9be8140dfaf220c76d81f87d6e02686081
Akira Kawamata <autumn@piedey.co.jp>
2022-12-21 12:02:09 +0900

    add GroupJoin1

---
+++ GroupJoin1/GroupJoin1/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] a = { "Anderson", "Brian", "Hanazono" };
+string[] b = { "Alice", "Antonio", "Hanako" };
+var q = a.GroupJoin(b, c => c[0], d => d[0], (x, e) => new { x, e });
+
+foreach (var n in q)
+    foreach (var m in n.e)
+        Console.WriteLine($"{m} {n.x}");
