240305dbf10f25d7b21a514165025e39bb6ec938
Akira Kawamata <autumn@piedey.co.jp>
2022-12-21 10:33:36 +0900

    add First1

---
+++ First1/First1/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] a = { 0, 1, 2, 3 };
+int r = a.FirstOrDefault(c => c % 2 != 0,-1);
+if (r == -1) Console.WriteLine("見つかりません。");
+else Console.WriteLine($"{string.Join(',', a)}から発見された最初の奇数は{r}です。");
