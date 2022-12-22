f8b4c2a64bb5a81ab990ed4c3e9b572d834de74f
Akira Kawamata <autumn@piedey.co.jp>
2022-12-22 11:52:16 +0900

    add SelectMany

---
+++ SelectMany1/SelectMany1/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] grades = { "小学1年", "小学2年", "中学1年" };
+string[] types1 = { "国語", "算数" };
+string[] types2 = { "国語", "数学", "英語" };
+
+var q = grades.SelectMany(
+    t => t.Contains("小") ? types1 : types2,
+    (g, t) => g + "の" + t
+    );
+foreach (var item in q) Console.WriteLine(item);
