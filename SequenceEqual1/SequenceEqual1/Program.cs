c3e58763675dca00bba49a21bb2a0763d8929262
Akira Kawamata <autumn@piedey.co.jp>
2022-12-22 16:16:57 +0900

    add SequenceEqual1

---
+++ SequenceEqual1/SequenceEqual1/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] a = { 1, 2, 3 };
+List<int> b = new List<int> { 1, 2, 3 };
+int[] c = { 1, 2, 4 };
+int[] d = { 1, 2, 3, 4 };
+int[] e = { 1, 2, 3 };
+
+Console.WriteLine($"aとbの比較結果: {a.SequenceEqual(b)}");
+Console.WriteLine($"aとcの比較結果: {a.SequenceEqual(c)}");
+Console.WriteLine($"aとdの比較結果: {a.SequenceEqual(d)}");
+Console.WriteLine($"aとeの比較結果: {a.SequenceEqual(e)}");
