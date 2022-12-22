266c63e081918957facd24ffd16264c45a5583cb
Akira Kawamata <autumn@piedey.co.jp>
2022-12-22 16:58:04 +0900

    add SequenceEqual3

---
+++ SequenceEqual3/SequenceEqual3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+A[] a1 = { new A("ALICE"), new A("BETTY") };
+A[] a2 = { new A("alice"), new A("betty") };
+
+Console.WriteLine(a1.SequenceEqual(a2,new Eq()));
+Console.WriteLine(a1.SequenceEqual(a2));
+
+class A
+{
+    public string Message { get; set; }
+    public A(string s) => Message = s;
+}
+
+class Eq : IEqualityComparer<A>
+{
+    public bool Equals(A? x, A? y) => x?.Message.ToLower() == y?.Message.ToLower();
+    public int GetHashCode([DisallowNull] A obj) => obj.GetHashCode();
+}
