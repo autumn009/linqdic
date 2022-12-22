a311bad4d2cc10d5e911bb04fa75844c1c7daf0e
Akira Kawamata <autumn@piedey.co.jp>
2022-12-22 11:11:53 +0900

    add Select1

---
+++ Select1/Select1/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] prices = { 100, 200, 300 };
+Console.WriteLine($"定価 {string.Join(',', prices)}");
+int[] discountedPrices = prices.Select(c => c * 8 / 10).ToArray();
+Console.WriteLine($"2割引価格 {string.Join(',', discountedPrices)}");
