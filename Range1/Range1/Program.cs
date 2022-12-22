1477a838baf474be9fa967b6933a5977e87f6419
Akira Kawamata <autumn@piedey.co.jp>
2022-12-22 09:12:53 +0900

    add Range1

---
+++ Range1/Range1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("for文を使った古いやり方");
+for (int i = 0; i < 10; i++)
+{
+    if (i != 0) Console.Write(",");
+    Console.Write($"{i}");
+}
+Console.WriteLine();
+
+Console.WriteLine("Rangeメソッドを使ったやり方");
+Console.WriteLine($"{string.Join(',', Enumerable.Range(0, 10))}");
