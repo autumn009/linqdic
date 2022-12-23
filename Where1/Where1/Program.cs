0cf5ebed634b70358277e5199f659b5cd54a081d
Akira Kawamata <autumn@piedey.co.jp>
2022-12-23 16:45:43 +0900

    add Where1

---
+++ Where1/Where1/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] a = { 1, 4, 2, 3, 6, 5 };
+Console.WriteLine($"元データ{string.Join(',', a)}");
+Console.WriteLine($"3より大きな値のみ{string.Join(',', a.Where(c => c > 3))}");
+Console.WriteLine($"偶数番目のみ{string.Join(',', a.Where((c, i) => i % 2 == 1))}");
