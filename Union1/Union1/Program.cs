1674bedfa101a7511fd44b2f59821d2144b789c2
Akira Kawamata <autumn@piedey.co.jp>
2022-12-23 15:33:57 +0900

    add Union1

---
+++ Union1/Union1/Program.cs
@@ -0,0 +1,4 @@
+﻿string[] danceClub = { "Hanako", "Misa", "Eriko" };
+string[] ikebanaClub = { "Eriko", "Misuzu", "Misa", "Michie" };
+
+Console.WriteLine($"ダンスクラブと生け花クラブのどちらかに所属しているのは{string.Join(',', danceClub.Union(ikebanaClub))}。");
