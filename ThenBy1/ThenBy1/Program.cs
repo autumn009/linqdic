67420c872973d0cf7a29d6122fa21403a5a2ad15
Akira Kawamata <autumn@piedey.co.jp>
2022-12-23 11:33:32 +0900

    add Then1

---
+++ ThenBy1/ThenBy1/Program.cs
@@ -0,0 +1,12 @@
+﻿Person[] p = { 
+    new Person("Taro",20,170),
+    new Person("Jiro",18,175),
+    new Person("Hanako",20,160),
+};
+
+foreach (var item in p.OrderBy(c => c.Age).ThenBy(c => c.Tall))
+{
+    Console.WriteLine($"{item.Name} 年齢{item.Age} 身長{item.Tall}cm");
+}
+
+record Person(string Name, int Age, int Tall);
