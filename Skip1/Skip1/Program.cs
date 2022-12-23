4a3223d0fa32ebf326a87b28882e9202ca21b7c8
Akira Kawamata <autumn@piedey.co.jp>
2022-12-23 10:28:48 +0900

    add Skip1

---
+++ Skip1/Skip1/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] a = { 1, 2, 3, 4, 5, 4, 3, 2, 1 };
+Console.WriteLine($"元のシーケンス: {string.Join(',', a)}");
+Console.WriteLine($"Skip(4): {string.Join(',', a.Skip(4))}");
+Console.WriteLine($"SkipLast(4): {string.Join(',', a.SkipLast(4))}");
+Console.WriteLine($"SkipWhile(c=>c<5): {string.Join(',', a.SkipWhile(c => c < 5))}");
