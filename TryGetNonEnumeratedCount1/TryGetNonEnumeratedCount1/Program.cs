7869b91b57e7a12867179e498e3227014b4acc22
Akira Kawamata <autumn@piedey.co.jp>
2022-12-23 14:42:12 +0900

    add TryGetNonEnumeratedCount1

---
+++ TryGetNonEnumeratedCount1/TryGetNonEnumeratedCount1/Program.cs
@@ -0,0 +1,21 @@
+﻿var a = Enumerable.Range(1, 5);
+sub(a, nameof(a));
+var a2 = Enumerable.Range(1, 5).Skip(1);
+sub(a2, nameof(a2));
+int[] b = { 1, 2, 3, 4, 5 };
+sub(b, nameof(b));
+var c = b.ToList();
+sub(c, nameof(c));
+sub(myEnum(), nameof(myEnum));
+
+IEnumerable<int> myEnum()
+{
+    for (int i = 0; i < 5; i++) yield return i;
+}
+
+void sub(IEnumerable<int> enu, string label)
+{
+    var r = enu.TryGetNonEnumeratedCount(out int count);
+    if (r) Console.WriteLine($"{label}の要素数は{count}");
+    else Console.WriteLine($"{label}の要素数は計測不能");
+}
