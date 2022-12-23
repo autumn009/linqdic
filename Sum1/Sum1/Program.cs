512a14bc8bcfe7bc8eed007a319332e10bc68bcc
Akira Kawamata <autumn@piedey.co.jp>
2022-12-23 11:06:41 +0900

    add Sum1

---
+++ Sum1/Sum1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] a = { 1, 2, 3 };
+Console.WriteLine(a.Sum());
+
+NameAndValue[] b = new NameAndValue[]
+{
+    new NameAndValue("Hanako",1.0),
+    new NameAndValue("Taro",2.5),
+    new NameAndValue("Maiko",0.2),
+};
+Console.WriteLine(b.Sum(c => c.Value));
+
+record NameAndValue(string Name, double Value );
