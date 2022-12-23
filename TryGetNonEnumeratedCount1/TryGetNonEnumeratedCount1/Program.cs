171d0b6b4706dc1a70e4a52b22c9039f911db9a6
Akira Kawamata <autumn@piedey.co.jp>
2022-12-23 14:49:32 +0900

    add more sample

--- TryGetNonEnumeratedCount1/TryGetNonEnumeratedCount1/Program.cs
+++ TryGetNonEnumeratedCount1/TryGetNonEnumeratedCount1/Program.cs
@@ -2,6 +2,8 @@
 sub(a, nameof(a));
 var a2 = Enumerable.Range(1, 5).Skip(1);
 sub(a2, nameof(a2));
+var a3 = Enumerable.Range(1, 5).SkipWhile(c=>c>0);
+sub(a3, nameof(a3));
 int[] b = { 1, 2, 3, 4, 5 };
 sub(b, nameof(b));
 var c = b.ToList();
